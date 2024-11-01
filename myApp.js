3b9eae92ceed1950e589261ae67f3128440fffbd
M-Asaduzzaman <asaduzzamanforhad6@gmail.com>
2024-11-01 15:20:25 +0600

    xssFilter added

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: "deny" }));
+app.use(xssFilter());
 
 module.exports = app;
 const api = require("./server.js");
